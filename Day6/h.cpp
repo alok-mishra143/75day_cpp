404257e58cd86626489c0e37903afea0c5400131
alok_mishra14 <143alokmishra143@gmail.com>
2023-12-06 22:30:42 +0530

    leetcode +GFG

---
+++ Day6/h.cpp
@@ -0,0 +1,9 @@
+// write a function of factorial code 
+// 5! = 5*4*3*2*1 = 120
+//  
+
+// int factorial(int n){  
+//     int fact=1;
+//     for(int i=1;i<=n;i++){
+//         fact=fact*i;
+//     }
---
+++ GFG How Many X's.cpp
@@ -0,0 +1,72 @@
+/*
+Given two integers L, R, and digit X. Find the number of occurrences of X in all the numbers in the range (L, R) excluding L and R.
+
+Example 1:
+
+Input:
+L=10, R=19, X=1
+Output:
+9
+Explanation:
+There are 9 1s (11, 12, 13, 14, 15, 16, 17, 18) in the numbers in the range (10,19).
+Example 2:
+
+Input:
+L=18, R=81, X=9
+Output:
+7
+Explanation:
+There are 7 occurrences of the digit 9 in the numbers in the range (18,81).
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function countX() which takes three integers L, R, and X as input parameters and returns the number of occurrences of X in the numbers in the range(L, R).
+
+Expected Time Complexity:O(RLogR)
+Expected Auxillary Space:O(1)
+*/
+
+// #solution
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countX(int L, int R, int X) {
+        // code here
+        int cnt=0;
+        for(int i=L+1;i<R;i++){
+            
+            int temp=i;
+            
+            while(temp){
+                int rem=temp%10;
+                if(rem==X)cnt++;
+                
+                temp=temp/10;
+            }
+        }
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int L, R, X;
+        cin >> L >> R >> X;
+        Solution ob;
+        int ans = ob.countX(L, R, X);
+        cout << ans << "\n";
+    }
+}
+
+// } Driver Code Ends
